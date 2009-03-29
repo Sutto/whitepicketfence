485711c3875f432be3dd5b6861bb4e43b0723f12
Darcy Laycock <sutto@sutto.net>
2009-03-29 19:18:48 +0800

    Dispatch hooks

--- incoming.rb
+++ incoming.rb
@@ -1,4 +1,6 @@
 require File.join(File.dirname(__FILE__), "server_tools")
+gem 'taf2-curb'
+require 'curl'
 
 $timeout = (ENV["LOOP_DELAY"] || 5).to_i
 
@@ -15,9 +17,25 @@ class WPFReceiver
   end
 
   def incoming(from, datetime, message)
-    SMS.receive(:contents => message, :number => from)
-    # TODO: Dispatch hooks here.
+    sms = SMS.receive(:contents => message, :number => from)
+    json = {
+      "number"   => from,
+      "contents" => message,
+      "id"       => sms.id,
+      "path"     => "/messages/#{sms.id}"
+    }.to_json
+    dispatch_json(json)
   end
+  
+  protected
+  
+  def dispatch_json(payload)
+    data = Curl::PostField.content('payload', payload)
+    settings['hook_urls'].each do |url|
+      Curl::Easy.http_post(url, data) rescue nil
+    end
+  end
+  
 end
 
 WPFReceiver.new($modem)
